b584b09684d88004e939e14cc69e39415745eb54
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-26 09:17:16 +0100

    Insert data into the animals and vets tables

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
+
+INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
